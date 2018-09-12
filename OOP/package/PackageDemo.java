462ae641db3c6eb3e119bccb2f27427c41f6e080
askwuxue <972523419@qq.com>
2018-09-12 14:30:56 +0800

    add package/

---
+++ OOP/package/PackageDemo.java
@@ -0,0 +1,7 @@
+package abc.xyz;    // 必须放在所有代码之前 javac -d . .java;   java 包名.类名
+
+public class PackageDemo {
+    public static void main (String[] args) {
+        System.out.println("package.....");
+    }
+}
